fc59a4629e262e335efc2b75adcd11b00561f7da
venkat-lingala <lvenkatkumarreddy@gmail.com>
2017-06-10 00:28:40 -0400

    Added support for nicknames

--- server.js
+++ server.js
@@ -3,19 +3,31 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 app.get('/',(req,res) => {
-res.sendFile(__dirname + "/index.html");
+res.sendFile(__dirname + "/nickname.html");
 });
 
+app.get('/chat',(req,res) => {
+res.sendFile(__dirname + "/index.html");
+});
+var nickName;
 io.on('connection',(socket) => {
 console.log("user connected");
- io.emit('start connection', 'welcome to public chat');
-socket.on('chat message',(msg) => {
- console.log(msg)
- io.emit('chat message', msg);
+
+
+socket.on('nick name',(nick) => {
+ nickName = nick;
+ console.log(nickName);
 });
+io.emit('start connection', nickName +' ' + 'joined the chat');
+
+socket.on('chat message',(msg) => {
+ io.emit('chat message','@' + nickName + " " + msg);
+});
+
 socket.on('disconnect',() => {
  console.log("user disconnected");
 });
+
 });
 
 http.listen(3000, () => {
