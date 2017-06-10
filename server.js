b95760c2b78702d25d6380edd084bc5f8c1bad3c
venkat-lingala <lvenkatkumarreddy@gmail.com>
2017-06-09 23:31:02 -0400

    socket connections created

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ res.sendFile(__dirname + "/index.html");
 
 io.on('connection',(socket) => {
 console.log("user connected");
+socket.on('disconnect',() => {
+ console.log("user disconnected");
+});
 });
 
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
    
