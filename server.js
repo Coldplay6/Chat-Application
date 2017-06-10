28ade491bcc8c9640815edb7b1115dc55a380d9f
venkat-lingala <lvenkatkumarreddy@gmail.com>
2017-06-10 14:59:23 -0400

    issue resolved

--- server.js
+++ server.js
@@ -7,36 +7,32 @@ var io = require("socket.io")(http);
 var chatHistory = [];
 
 app.get('/',(req,res) => {
-res.sendFile(__dirname + "/nickname.html");
-});
-
-app.get('/chat',(req,res) => {
 res.sendFile(__dirname + "/index.html");
 });
-var nickName;
+
 io.on('connection',(socket) => {
 console.log("user connected");
 
-
+var nickName = 'new user';
 socket.on('nick name',(nick) => {
  nickName = nick;
  console.log(nickName);
 });
 
-io.emit('start connection', nickName +' ' + 'joined the chat');
-io.emit('chat history', chatHistory);
+console.log(nickName);
+socket.broadcast.emit('start connection', nickName +' ' + 'joined the chat');
+
+socket.emit('chat history', chatHistory);
 
 socket.on('chat message',(msg) => {
- io.emit('chat message','@' + nickName + " " + msg);
+ io.emit('chat message', nickName + ":" + " " + msg);
  if (_.size(chatHistory) > 10) {  
   chatHistory.splice(0,1);
 } else {
-  chatHistory.push('@' + nickName + " " + msg);
+  chatHistory.push(nickName + ":" + " " + msg);
 }
 console.log(chatHistory);
 });
-
-
 
 socket.on('disconnect',() => {
  io.emit('end connection', nickName +' ' + 'left the chat');
@@ -44,7 +40,7 @@ socket.on('disconnect',() => {
 
 });
 
-http.listen(3000, () => {
+http.listen(3000,0, () => {
   console.log('listening on *:3000');
 });
    
